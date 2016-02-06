a8b286583fe9866b30ab5d46e7ee648c79584c24
Max Kaplan <mgkaplan96@gmail.com>
2016-02-06 18:27:46 -0500

    hopefully got rid of the login thing

--- app/src/main/java/comquintonjcinemasnob/github/cinemasnob/MainActivity.java
+++ app/src/main/java/comquintonjcinemasnob/github/cinemasnob/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         Button submitLogin = (Button) findViewById(R.id.submit_login);
         submitLogin.setOnClickListener(new View.OnClickListener() {
                                           public void onClick(View view) {
-                                              Snackbar.make(view, "Message", Snackbar.LENGTH_LONG).setAction("Action", null).show();                                          }
+                                              Snackbar.make(view, "Message", Snackbar.LENGTH_LONG).setAction("Action", null).show();}
                                        });
 
         UserManagement manager = new UserManager();
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
             Intent goToHomeScreen = new Intent(this, HomeScreen.class);
             startActivity(goToHomeScreen);
         } else {
-            failedLogin = "Incorrect Username or Password, please try again.";
+            failedLogin = "Incorrect Username or Password, try again.";
             Context context = getApplicationContext();
             int duration = Toast.LENGTH_SHORT;
             Toast fail = Toast.makeText(context, failedLogin, duration);
