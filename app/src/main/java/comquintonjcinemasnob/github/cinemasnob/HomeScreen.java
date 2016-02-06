379f5ba3d35fd499cf75de8eaf3a67ec9e26582c
Erik Maday <erikammaday@gmail.com>
2016-02-06 16:10:03 -0500

    added main screen with logout button

---
+++ app/src/main/java/comquintonjcinemasnob/github/cinemasnob/HomeScreen.java
@@ -0,0 +1,18 @@
+package comquintonjcinemasnob.github.cinemasnob;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+    }
+
+}
