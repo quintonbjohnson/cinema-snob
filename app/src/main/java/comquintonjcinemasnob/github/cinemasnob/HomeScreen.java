d3a0a69f589faba3f9ba5fb7e5553f858aa24c6c
Quinton <qjohnson@gatech.edu>
2016-02-07 20:31:07 -0500

    Logout button works

--- app/src/main/java/comquintonjcinemasnob/github/cinemasnob/HomeScreen.java
+++ app/src/main/java/comquintonjcinemasnob/github/cinemasnob/HomeScreen.java
@@ -1,11 +1,10 @@
 package comquintonjcinemasnob.github.cinemasnob;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -13,5 +12,15 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+
+        // Logout button sends back to login screen
+        Button logout = (Button)findViewById(R.id.logout_button);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeScreen.this, MainActivity.class));
+            }
+        });
     }
 }
