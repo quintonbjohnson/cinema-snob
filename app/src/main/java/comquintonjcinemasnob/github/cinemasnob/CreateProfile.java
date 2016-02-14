8bbe031ace277580c0d49685fd2d8f1e527981ee
BranSeal <BranSeal@users.noreply.github.com>
2016-02-14 16:55:09 -0500

    fixed db

--- app/src/main/java/comquintonjcinemasnob/github/cinemasnob/CreateProfile.java
+++ app/src/main/java/comquintonjcinemasnob/github/cinemasnob/CreateProfile.java
@@ -66,7 +66,7 @@ public class CreateProfile extends AppCompatActivity {
             fail.show();
         } else {
             userdb = new UserOpenHelper(context);
-            userdb.putUser(userdb, usernameBox.toString(), passwordBox.toString(), emailBox.toString());
+            userdb.putUser(userdb, usernameBox.getText().toString(), passwordBox.getText().toString(), emailBox.getText().toString());
             Toast.makeText(getBaseContext(), "Successfully registered!", Toast.LENGTH_LONG).show();
 
             manager.addUser(usernameBox.getText().toString(), passwordBox.getText().toString(),
