5dd4b96c759d3d052f150fd398a16b7b1cb77413
Jacob Goodpasture <jrgoodpasture@gmail.com>
2016-02-14 15:46:53 -0500

    Added javadocs

--- app/src/main/java/comquintonjcinemasnob/github/cinemasnob/User.java
+++ app/src/main/java/comquintonjcinemasnob/github/cinemasnob/User.java
@@ -9,12 +9,23 @@ public class User {
     String password;
     String email;
 
+    /**
+     * Instantiates the User object
+     * @param n Username
+     * @param p Password
+     * @param e Email
+     */
     public User(String n, String p, String e ) {
         userName = n;
         password = p;
         email = e;
     }
 
+    /**
+     * Checks if password provided matches actual password
+     * @param pass Password provided by user
+     * @return True or false if password matches or does not match
+     */
     public boolean checkPassword(String pass) {
         return password.equals(pass);
     }
