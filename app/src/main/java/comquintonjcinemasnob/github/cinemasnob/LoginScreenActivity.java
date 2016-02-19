4b8b6c0f9dd6a6b813c91323c5da72533d8d2261
Quinton <qjohnson@gatech.edu>
2016-02-19 18:10:13 -0500

    Recent DVDs

--- app/src/main/java/comquintonjcinemasnob/github/cinemasnob/LoginScreenActivity.java
+++ app/src/main/java/comquintonjcinemasnob/github/cinemasnob/LoginScreenActivity.java
@@ -21,7 +21,7 @@ public class LoginScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login_screen);
         context = this;
 
         // Login
