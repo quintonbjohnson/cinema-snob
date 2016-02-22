3f2273b6e2e4920e4565c79ef2b96b6b18197713
branseal <BranSeal@users.noreply.github.com>
2016-02-21 19:57:33 -0500

    Added javadoc for RequestController.java

--- app/src/main/java/project/github/cinemasnob/RequestController.java
+++ app/src/main/java/project/github/cinemasnob/RequestController.java
@@ -21,10 +21,18 @@ public class RequestController extends Application {
         instance = this;
     }
 
+    /**
+     * Gets the current instance of the application
+     * @return RequestController of the current instance
+     */
     public static synchronized RequestController getInstance() {
         return instance;
     }
 
+    /**
+     * Get method that returns the request queue
+     * @return request queue for calling the API and returning JSON object
+     */
     public RequestQueue getRequestQueue() {
         if (rq == null) {
             rq = Volley.newRequestQueue(getApplicationContext());
@@ -32,16 +40,31 @@ public class RequestController extends Application {
         return rq;
     }
 
+    /**
+     * Adds a request to the request queue
+     * @param req request to be added to queue
+     * @param tag tag name for adding
+     * @param <T> any type of request
+     */
     public <T> void addToRequestQueue(Request<T> req, String tag) {
         req.setTag(TAG);
         getRequestQueue().add(req);
     }
 
+    /**
+     * Adds a request to the request queue
+     * @param req request to be added to queue
+     * @param <T> any type of request
+     */
     public <T> void addToRequestQueue(Request<T> req) {
         req.getTag();
         getRequestQueue().add(req);
     }
 
+    /**
+     * Cancels all pending requests currently in the request queue
+     * @param tag the tag specified will have all requests under this tag removed
+     */
     public void cancelPendingRequests(Object tag) {
         if (rq != null) {
             rq.cancelAll(tag);
