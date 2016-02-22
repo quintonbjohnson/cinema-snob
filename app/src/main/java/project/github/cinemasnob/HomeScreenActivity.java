9324a6ca5470d759ef91006dcfcc5ef3f4fb8a1f
Jacob Goodpasture <jrgoodpasture@gmail.com>
2016-02-22 18:54:00 -0500

    Changed Recent Movie and Recent DVD buttons so that they show the information on the HomeScreen listview rather than changing activities

--- app/src/main/java/project/github/cinemasnob/HomeScreenActivity.java
+++ app/src/main/java/project/github/cinemasnob/HomeScreenActivity.java
@@ -32,7 +32,6 @@ public class HomeScreenActivity extends AppCompatActivity {
     private static final String API_KEY = "yedukp76ffytfuy24zsqk7f5";
     private static final int MOVIE_PAGE_LIMIT = 10;
     private ListView movieList;
-    private TextView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,20 +69,118 @@ public class HomeScreenActivity extends AppCompatActivity {
         recentMovies.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent goToRecentMovie = new Intent(HomeScreenActivity.this,
-                        RecentMoviesActivity.class);
-                startActivity(goToRecentMovie);
+                movieList = (ListView)findViewById(R.id.movieList);
+                final String movieSearchString = "Recent Movies";
+                final MovieList listOfMovies = new MovieList();
+                String url = "";
+                try {
+                    url = "http://api.rottentomatoes.com/api/public/v1.0/" +
+                            "lists/movies/in_theaters.json?page_limit=15&page=1&country=us&apikey="
+                            + API_KEY;
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+                JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                        url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //create new movieList
+                        //MovieList listOfMovies = new MovieList();
+                        try {
+                            JSONArray movies = response.getJSONArray("movies");
+                            //one single movie
+                            JSONObject movie = null;
+                            //Test block to see how many movies
+                            // there are that are returned in the JSON object
+                            for (int i = 0; i < movies.length(); i++) {
+                                movie = movies.getJSONObject(i);
+                                //create a new movie
+                                Movie newMovie = new Movie(movie.getString("title"),
+                                        movie.getString("year"),
+                                        movie.getString("mpaa_rating"));
+                                listOfMovies.addMovie(newMovie);
+                                //will print out all the titles of the movies
+                                // that were returned from the REST call search
+                                Log.d("Movie Object: ", listOfMovies.getTitleList().get(i));
+                            }
+                            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                                    HomeScreenActivity.this,
+                                    android.R.layout.simple_list_item_1,
+                                    listOfMovies.getTitleList());
+                            movieList.setAdapter(arrayAdapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(), "Error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d("Ohfuck.jpg", "We done goofed");
+                            }
+                        });
+                RequestController.getInstance().addToRequestQueue(jsonObjReq);
             }
         });
 
-        // RecentDVDs button sends to recent DVDs
+        // RecentDVDs button sends to displays DVDs
         Button recentDVD = (Button)findViewById(R.id.recentDVD_button);
 
         recentDVD.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent goToRecentDVD= new Intent(HomeScreenActivity.this, RecentDVDActivity.class);
-                startActivity(goToRecentDVD);
+                movieList = (ListView)findViewById(R.id.movieList);
+                final String movieSearchString = "Recent Movies";
+                final MovieList listOfMovies = new MovieList();
+                String url = "";
+                try {
+                    url = "http://api.rottentomatoes.com/api/public/v1.0/" +
+                            "lists/dvds/new_releases.json?page_limit=15&page=1&country=us&apikey="
+                            + API_KEY;
+                } catch (Exception e){
+                    e.printStackTrace();
+                }
+                JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
+                        url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        // Create new movieList
+                        // MovieList listOfMovies = new MovieList();
+                        try {
+                            JSONArray movies = response.getJSONArray("movies");
+                            // One single movie
+                            JSONObject movie = null;
+                            // Iterating through JSON objects
+                            for (int i = 0; i < movies.length(); i++) {
+                                movie = movies.getJSONObject(i);
+                                // Create a new movie
+                                Movie newMovie = new Movie(movie.getString("title"),
+                                        movie.getString("year"),
+                                        movie.getString("mpaa_rating"));
+                                listOfMovies.addMovie(newMovie);
+                            }
+                            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(HomeScreenActivity.this,
+                                    android.R.layout.simple_list_item_1,
+                                    listOfMovies.getTitleList());
+                            movieList.setAdapter(arrayAdapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(), "Error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Log.d("Error", "We done goofed");
+                            }
+                        });
+                RequestController.getInstance().addToRequestQueue(jsonObjReq);
             }
         });
 
