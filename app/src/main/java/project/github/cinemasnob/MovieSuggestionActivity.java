e31cf690cc3adb71ad0d5d62eea01ef95c2585a8
Quinton <qjohnson@gatech.edu>
2016-03-05 22:14:51 -0500

    MovieSuggestionActivity created

--- app/src/main/java/project/github/cinemasnob/MovieSuggestionActivity.java
+++ app/src/main/java/project/github/cinemasnob/MovieSuggestionActivity.java
@@ -3,6 +3,7 @@ package project.github.cinemasnob;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 
@@ -12,6 +13,10 @@ public class MovieSuggestionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_suggestion);
+
+        Button majorButton = (Button)findViewById(R.id.major_button);
+        Button yearButton = (Button)findViewById(R.id.year_button);
+        Button overallButton = (Button)findViewById(R.id.overall_button);
 
     }
 
