04132cbdcb802e62e05c67c2153e9b4e33c654cb
Quinton <qjohnson@gatech.edu>
2016-03-06 21:36:42 -0500

    Recommendation now works

--- app/src/main/java/project/github/cinemasnob/MovieSuggestionActivity.java
+++ app/src/main/java/project/github/cinemasnob/MovieSuggestionActivity.java
@@ -1,16 +1,20 @@
 package project.github.cinemasnob;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.content.Context;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 
 /**
  * Movie suggestions
@@ -19,6 +23,7 @@ public class MovieSuggestionActivity extends AppCompatActivity {
 
     private RatingOpenHelper ratingdb;
     private ListView sortedMovieList;
+    private HashMap<String, Integer> movieIds = new HashMap<String, Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +51,7 @@ public class MovieSuggestionActivity extends AppCompatActivity {
                 ArrayList<String> titles = new ArrayList<String>();
                 for (MovieHelper help : movies) {
                     titles.add(help.getTitle());
+                    movieIds.put(help.getTitle(), help.getId());
                 }
 
                 // Populate the ListView
@@ -54,6 +60,21 @@ public class MovieSuggestionActivity extends AppCompatActivity {
                         android.R.layout.simple_list_item_1,
                         titles);
                 sortedMovieList.setAdapter(arrayAdapter);
+            }
+        });
+
+        sortedMovieList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // Selected item
+                String title = ((TextView) view).getText().toString();
+                int movieID = movieIds.get(title);
+                // Launching new Activity on selecting single List Item
+                Intent goToMovieItem = new Intent(getApplicationContext(), MovieItemActivity.class);
+                // Sending data to new activity
+                goToMovieItem.putExtra("ID", movieID);
+                startActivity(goToMovieItem);
             }
         });
     }
