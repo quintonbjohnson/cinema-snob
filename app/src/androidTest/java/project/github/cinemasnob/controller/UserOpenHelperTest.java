9fcbab8ef9b49f512c73cfcbffb0bc42d6d80576
Erik Maday <erikammaday@gmail.com>
2016-04-04 18:35:51 -0400

    added JUnit tests for putUser

--- app/src/androidTest/java/project/github/cinemasnob/controller/UserOpenHelperTest.java
+++ app/src/androidTest/java/project/github/cinemasnob/controller/UserOpenHelperTest.java
@@ -1,13 +1,68 @@
 package project.github.cinemasnob.controller;
 
-import junit.framework.TestCase;
+import android.content.Context;
 
-/**
- * Created by erikmaday on 4/4/16.
- */
-public class UserOpenHelperTest extends TestCase {
+import org.junit.Test;
 
+import project.github.cinemasnob.model.User;
+
+import static org.junit.Assert.*;
+
+public class UserOpenHelperTest {
+
+    private Context mockContext;
+    private UserOpenHelper userDB;
+
+    @Test
     public void testPutUser() throws Exception {
+        userDB = new UserOpenHelper(mockContext);
 
+        boolean fail;
+        boolean succeed;
+
+        //null username
+        fail = userDB.putUser(userDB, null, "pass", "email@email.com", "major", false);
+        assertFalse(fail);
+
+        //blank username
+        fail = userDB.putUser(userDB, "", "pass", "email@email.com", "major", false);
+        assertFalse(fail);
+
+        //null password
+        fail = userDB.putUser(userDB, "username", null, "email@email.com", "major", false);
+        assertFalse(fail);
+
+        //blank password
+        fail = userDB.putUser(userDB, "username", "", "email@email.com", "major", false);
+        assertFalse(fail);
+
+        //null email
+        fail = userDB.putUser(userDB, "username", "pass", null, "major", false);
+        assertFalse(fail);
+
+        //blank email
+        fail = userDB.putUser(userDB, "username", "pass", "", "major", false);
+        assertFalse(fail);
+
+        // success scenarios
+        try {
+            succeed = userDB.putUser(userDB, "username", "pass",
+                    "email@email.com", "major", false);
+            assertTrue(succeed);
+        } catch (NullPointerException e) {
+            System.out.println("Insert uses a null input value.");
+        }
+
+        try {
+            User testUser =
+                    userDB.getUser(userDB, "username");
+            assertEquals(testUser.getUserName(), "username");
+            assertEquals(testUser.getPassword(), "password");
+            assertEquals(testUser.getEmail(), "email@email.com");
+            assertEquals(testUser.getMajor(), "major");
+            assertEquals(testUser.getBanStatus(), false);
+        } catch (NullPointerException e) {
+            System.out.println("Insert uses a null input value.");
+        }
     }
 }
